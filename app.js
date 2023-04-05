294d3167d0a69eafb1ed2615abfc15c38139269f
Padraic Reilly <patrick.reilly22@gmail.com>
2023-04-05 14:55:43 -0700

    added scrolling effect

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const body = document.body;
 window.addEventListener('scroll', () => {
-  const scrollPos = window.scrollY;
-  body.style.backgroundPositionX = `${-scrollPos}px`;
+  body.style.backgroundPositionX = `${-window.scrollY}px`;
 });
