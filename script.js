3b31cf0b329ef8849b25c2e1cf8a51b14402547a
Padraic Reilly <patrick.reilly22@gmail.com>
2023-12-09 18:53:25 -0800

    Testing deployment

--- script.js
+++ script.js
@@ -6,3 +6,46 @@ window.addEventListener('scroll', () => {
     else if (scrollPercentage < 0) { scrollPercentage = 0; }
     body.style.backgroundPositionX = `${scrollPercentage}%`;
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const elements = document.querySelectorAll('.header-card, .about-card, .project-card, .skill-category, .contact-card');
+    const windowHeight = window.innerHeight;
+
+    function isInView(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top <= windowHeight &&
+            rect.bottom >= 0
+        );
+    }
+
+    function checkPosition() {
+        for (let i = 0; i < elements.length; i++) {
+            if (isInView(elements[i])) {
+                elements[i].classList.add('in-view');
+            }
+            else {
+                elements[i].classList.remove('in-view');
+            }
+        }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    checkPosition();
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    async function getProfilePicURL() {
+        const profilePicElement = document.getElementById("profile-picture");
+        const githubBioElement = document.getElementById("github-bio");
+        const response = await fetch("https://api.github.com/users/PER22");
+        const githubUserObject = await response.json();
+        const profilePicURL = githubUserObject["avatar_url"];
+        const bioString = githubUserObject["bio"];
+        profilePicElement.setAttribute("src", profilePicURL);
+        githubBioElement.innerText = bioString;
+    }
+    getProfilePicURL();
+
+});
