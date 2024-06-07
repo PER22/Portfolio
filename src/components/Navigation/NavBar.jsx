32161f66cf61169f1199a50ad7af3ec07dc917e8
Padraic Reilly <patrick.reilly22@gmail.com>
2024-06-06 23:03:52 -0700

    change lighting

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -4,7 +4,7 @@ import './NavBar.css'
 export default function NavBar() {
     return (<>
         <nav aria-label="Main navigation" className="nav-container">
-            <NavLink className="nav-link" to="/Portfolio">Skills</NavLink>
+            <NavLink className="nav-link" to="/Portfolio/">Skills</NavLink>
             <NavLink className="nav-link" to="/about">About</NavLink>
             <NavLink className="nav-link" to="/projects">Projects</NavLink>
         </nav>
--- src/pages/Home/components/SummaryHeader/components/SocialMediaLinks.jsx
+++ src/pages/Home/components/SummaryHeader/components/SocialMediaLinks.jsx
@@ -3,16 +3,16 @@ export default function SocialMediaLinks() {
 
     return (<div id="social-media-container">
         <a className="social-media-link" target="_blank" rel="noreferrer" href="https://linkedin.com/in/padraic-reilly">
-            <img className="social-media-logo" src="/logos/linkedin_logo.png" alt="LinkedIn Logo" />
+            <img className="social-media-logo" src="logos/linkedin_logo.png" alt="LinkedIn Logo" />
         </a>
         <a className="social-media-link" target="_blank" rel="noreferrer" href="https://github.com/PER22">
-            <img className="social-media-logo" src="/logos/github_logo.png" alt="GitHub Logo" />
+            <img className="social-media-logo" src="logos/github_logo.png" alt="GitHub Logo" />
         </a>
         <a className="social-media-link" target="_blank" rel="noreferrer" href="resume/Padraic-Reilly-Resume.pdf" download="Padraic-Reilly-Resume.pdf">
-            <img className="social-media-logo" src="/logos/resume_logo.png" alt="Resume Logo" />
+            <img className="social-media-logo" src="logos/resume_logo.png" alt="Resume Logo" />
         </a>
         <a className="social-media-link" target="_blank" rel="noreferrer" href="https://leetcode.com/u/PadraicReilly/">
-            <img className="social-media-logo" src="/logos/leetcode_logo.png" alt="LeetCode Logo" />
+            <img className="social-media-logo" src="logos/leetcode_logo.png" alt="LeetCode Logo" />
         </a>
     </div>);
 }
